4af4049ef8c862baaa60bdea5b95a10365efb91f
Daniel <angus9209@gmail.com>
2022-04-27 20:38:28 +1200

    Completed setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,10 @@ module.exports = {
     title: `caron-automotive`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-sass"]
+  plugins: [
+    "gatsby-plugin-sass",
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+  ]
 };
