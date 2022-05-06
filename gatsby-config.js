9ebc8744650ca7c1af5b0fcb1575e92b4c58ece6
Daniel <angus9209@gmail.com>
2022-05-06 20:53:00 +1200

    inject google fonts using gatsby-plugin-google-fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,15 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Montserrat:500,600`,
+          `source sans pro:300,400,400i,700` // you can also specify font weights and styles
+        ],
+        display: 'swap'
+      }
+    }
   ]
 };
